bbd4c8480a6f46fea787d7b8e639a5947bcb4965
vishal Jangid <vishaljangid80550786@gmail.com>
2026-04-16 09:20:38 +0530

    cors updation

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
-import aiRouter from "./routes/aiRoutes.js";
-import connectCloudinary from "./configs/cloudinary.js";
-import userRouter from "./routes/userRoutes.js";
+import aiRouter from "../routes/aiRoutes.js";
+import connectCloudinary from "../configs/cloudinary.js";
+import userRouter from "../routes/userRoutes.js";
 
 const app = express();
 await connectCloudinary();
@@ -12,25 +12,29 @@ await connectCloudinary();
 app.use(
   cors({
     origin: "https://quick-ai-frontend-green.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
-app.options("*", cors());
+
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "https://quick-ai-frontend-green.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
-  res.send("Server  is running!");
+  res.send("Server is running!");
 });
 
-app.use(requireAuth());
+app.use("/api", requireAuth());
 app.use("/api/ai", aiRouter);
 app.use("/api/user", userRouter);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
