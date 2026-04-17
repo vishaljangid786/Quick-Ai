e3f5f84b43a78bd5f24e29e8881d465706eb5980
vishal Jangid <vishaljangid80550786@gmail.com>
2026-04-17 14:36:32 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
-import aiRouter from "../routes/aiRoutes.js";
-import connectCloudinary from "../configs/cloudinary.js";
-import userRouter from "../routes/userRoutes.js";
+import aiRouter from "./routes/aiRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 await connectCloudinary();
