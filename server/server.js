9ccba7a00f80033f18afc05579617b42270b9048
vishal Jangid <vishaljangid80550786@gmail.com>
2026-04-17 14:39:14 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -10,12 +10,7 @@ const app = express();
 await connectCloudinary();
 
 app.use(
-  cors({
-    origin: ["https://quick-ai-frontend-green.vercel.app", "http://localhost:5173", "http://localhost:5174"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  cors()
 );
 
 app.use(express.json());
