81351d0a996f8aa18df72e5fbd6c173f44f73f37
vishal Jangid <vishaljangid80550786@gmail.com>
2026-04-17 14:38:08 +0530

    update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,7 +7,7 @@ const userRouter = express.Router();
 userRouter.get("/get-user-creations", auth, getUserCreations);
 
 
-userRouter.get("/get-published-creations", auth, getPublishedCreations);
+userRouter.get("/get-published-creations", getPublishedCreations);
 
 
 userRouter.post("/toggle-like-creations", auth, toggleLikeCreations);
